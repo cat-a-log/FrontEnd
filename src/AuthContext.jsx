11ebfb077c0c8f839e70829441528ebb0b652bea
Mariuxi <catmaluci@hotmail.com>
2025-05-17 17:50:34 +0200

    get current user

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,14 +1,33 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react";
 
-const AuthContext = createContext(null)
-export const AuthProvider = ({children}) => {
+const AuthContext = createContext(null);
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const getCurrentUser = async () => {
+    setLoading (true);
+    try {
+        const response = await fetch ("http://localhost:8080/api/me")
+        if (response.ok) {
+            const data = await response.json()
+            setUser (data);
+        } else {
+            setUser (null);
+        }
+    } catch (error) {
+        setUser (null);
+    } finally {
+        setLoading (false);
+    }
 
-    return (
-        <AuthContext.Provider>
-        {children}
-        </AuthContext.Provider> 
-    )
-}
+  };
+
+  useEffect(() => {
+    getCurrentUser ();
+  }, [])
+  return <AuthContext.Provider value = {{user, loading}}>{children}</AuthContext.Provider>;
+};
 export const useAuth = () => {
-    return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
+
